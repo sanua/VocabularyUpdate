ed6a5703256c763a72f98e4d78402235d2adc96c
Alexander N. Yatsenko <avatorv@gmail.com>
2017-03-16 22:32:24 +0200

    Fixed outputs, mock data usage

--- vocabulary_data/dev_hcpcs/mock/03.mock_load_stage.sql
+++ vocabulary_data/dev_hcpcs/mock/03.mock_load_stage.sql
@@ -15,7 +15,7 @@ PROMPT *** Step 'procedure_drug.sql' is started... ***
 -- Run HCPCS/procedure_drug.sql. This will create all the input files for MapDrugVocabulary.sql
 @&2/mock/mock_procedure_drug.sql '&3'
 PROMPT *** Step 'procedure_drug.sql' is done... ***
-
+PROMPT
 PROMPT *** Step 'MapDrugVocabulary.sql' id started... ***
 --Run the generic working/MapDrugVocabulary.sql. This will produce a concept_relationship_stage with HCPCS to RxNorm relatoinships
 @&2/mock/mock_MapDrugVocabulary.sql '&4'
