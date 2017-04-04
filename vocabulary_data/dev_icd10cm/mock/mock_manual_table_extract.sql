c6a0723cac2796a47661c95899a42390fc95c920
Alexander N. Yatsenko <avatorv@gmail.com>
2017-04-04 16:24:17 +0300

    Added conversion for exported DATE fields

--- vocabulary_data/dev_icd10cm/mock/mock_manual_table_extract.sql
+++ vocabulary_data/dev_icd10cm/mock/mock_manual_table_extract.sql
@@ -28,8 +28,8 @@ SELECT '"' || CONCEPT_ID  || '",'
     || '"' || CONCEPT_CLASS_ID || '",'
     || '"' || STANDARD_CONCEPT || '",'
     || '"' || CONCEPT_CODE || '",'
-    || '"' || VALID_START_DATE || '",'
-    || '"' || VALID_END_DATE || '",'
+    || '"' || TO_CHAR(VALID_START_DATE, "YYYY-MM-DD HH24:MI:SS") AS VALID_START_DATE || '",'
+    || '"' || TO_CHAR(VALID_END_DATE, "YYYY-MM-DD HH24:MI:SS") AS VALID_END_DATE || '",'
     || '"' || INVALID_REASON || '"'
 FROM &3
 ;
