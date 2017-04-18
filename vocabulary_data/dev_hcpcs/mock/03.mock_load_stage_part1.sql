7685b3105d7d14d6ca45db2b3f9ed5bfae12bbec
Alexander N. Yatsenko <avatorv@gmail.com>
2017-04-18 12:33:47 +0300

    Updated data to use test data.

--- vocabulary_data/dev_hcpcs/mock/03.mock_load_stage_part1.sql
+++ vocabulary_data/dev_hcpcs/mock/03.mock_load_stage_part1.sql
@@ -11,20 +11,6 @@ SPOOL &1
 PROMPT
 PROMPT 'Load Stage part 1' is starting...
 EXEC DBMS_LOCK.sleep(1);
-
-PROMPT *** Step 'procedure_drug.sql' is started... ***
--- Run HCPCS/procedure_drug.sql. This will create all the input files for MapDrugVocabulary.sql
-@&2/mock/mock_procedure_drug.sql '&3'
-PROMPT *** Step 'procedure_drug.sql' is done... ***
-
-PROMPT
-
-PROMPT *** Step 'MapDrugVocabulary.sql' is started... ***
---Run the generic working/MapDrugVocabulary.sql. This will produce a concept_relationship_stage with HCPCS to RxNorm relatoinships
-@&2/mock/mock_MapDrugVocabulary.sql '&4'
-prompt *** Step 'MapDrugVocabulary.sql' is done... ***
-
-EXEC DBMS_LOCK.sleep(1);
 PROMPT 'Load Stage part 1' is done...
 PROMPT
 
