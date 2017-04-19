8cae3823e93231f9b7e761e2e6deb0b0f2bedaa0
Alexander N. Yatsenko <avatorv@gmail.com>
2017-04-19 18:10:31 +0300

    Added ordering to extracted data from 'Manual Table'

--- vocabulary_data/dev_hcpcs/manual_table_extract.sql
+++ vocabulary_data/dev_hcpcs/manual_table_extract.sql
@@ -10,6 +10,19 @@ COLUMN exp_file_name new_val exp_file_name
 SELECT '&1'|| '/' || '&2' AS exp_file_name FROM dual;
 SPOOL &&exp_file_name
 
+WITH mt AS (SELECT CONCEPT_CODE_1,
+                   CONCEPT_NAME_1,
+                   CONCEPT_ID_2,
+                   CONCEPT_NAME_2,
+                   DOMAIN_ID,
+                   VOCABULARY_ID,
+                   INVALID_REASON,
+                   TO_CHAR(VALID_START_DATE, 'YYYY-MM-DD HH24:MI:SS') AS VALID_START_DATE,
+                   TO_CHAR(VALID_END_DATE, 'YYYY-MM-DD HH24:MI:SS') AS VALID_END_DATE,
+                   PRECEDENCE,
+                   CONVERSION_FACTOR
+            FROM &3
+            ORDER BY CONCEPT_CODE_1, CONCEPT_NAME_1, CONCEPT_ID_2, CONCEPT_NAME_2, DOMAIN_ID, VOCABULARY_ID, VALID_START_DATE, VALID_END_DATE)
 SELECT 'CONCEPT_CODE_1' || ','
     || 'CONCEPT_NAME_1' || ','
     || 'CONCEPT_ID_2' || ','
@@ -20,21 +33,21 @@ SELECT 'CONCEPT_CODE_1' || ','
     || 'VALID_START_DATE' || ','
     || 'VALID_END_DATE' || ','
     || 'PRECEDENCE' || ','
-    || 'CONVERSION_FACTOR'
+    || 'CONVERSION_FACTOR' AS "&3 DATA"
 FROM DUAL
 UNION ALL
-SELECT '"' || CONCEPT_CODE_1  || '",'
-    || '"' || CONCEPT_NAME_1 || '",'
-    || '"' || CONCEPT_ID_2 || '",'
-    || '"' || CONCEPT_NAME_2 || '",'
-    || '"' || DOMAIN_ID || '",'
-    || '"' || VOCABULARY_ID || '",'
-    || '"' || INVALID_REASON || '",'
-    || '"' || TO_CHAR(VALID_START_DATE, 'YYYY-MM-DD HH24:MI:SS') || '",'
-    || '"' || TO_CHAR(VALID_END_DATE, 'YYYY-MM-DD HH24:MI:SS')  || '",'
-    || '"' || PRECEDENCE || '",'
-    || '"' || CONVERSION_FACTOR || '"'
-FROM &3
+SELECT '"' || mt.CONCEPT_CODE_1  || '",'
+    || '"' || mt.CONCEPT_NAME_1 || '",'
+    || '"' || mt.CONCEPT_ID_2 || '",'
+    || '"' || mt.CONCEPT_NAME_2 || '",'
+    || '"' || mt.DOMAIN_ID || '",'
+    || '"' || mt.VOCABULARY_ID || '",'
+    || '"' || mt.INVALID_REASON || '",'
+    || '"' || mt.VALID_START_DATE || '",'
+    || '"' || mt.VALID_END_DATE || '",'
+    || '"' || mt.PRECEDENCE || '",'
+    || '"' || mt.CONVERSION_FACTOR || '"'
+FROM mt
 ;
 
 SPOOL OFF
--- vocabulary_data/dev_icd10cm/manual_table_extract.sql
+++ vocabulary_data/dev_icd10cm/manual_table_extract.sql
@@ -10,6 +10,21 @@ COLUMN exp_file_name new_val exp_file_name
 SELECT '&1'|| '/' || '&2' AS exp_file_name FROM dual;
 SPOOL &&exp_file_name
 
+WITH mt AS (SELECT CONCEPT_CODE_1,
+                   CONCEPT_NAME_1,
+                   VOCABULARY_ID_1,
+                   INVALID_REASON_1,
+                   CONCEPT_CODE_2,
+                   CONCEPT_NAME_2,
+                   CONCEPT_CLASS_ID_2,
+                   VOCABULARY_ID_2,
+                   INVALID_REASON_2,
+                   RELATIONSHIP_ID ,
+                   TO_CHAR(VALID_START_DATE, 'YYYY-MM-DD HH24:MI:SS') AS VALID_START_DATE,
+                   TO_CHAR(VALID_END_DATE, 'YYYY-MM-DD HH24:MI:SS') AS VALID_END_DATE,
+                   INVALID_REASON
+            FROM &3
+            ORDER BY CONCEPT_CODE_1, CONCEPT_NAME_1, VOCABULARY_ID_1, CONCEPT_CODE_2, CONCEPT_NAME_2, CONCEPT_CLASS_ID_2, VOCABULARY_ID_2, VALID_START_DATE, VALID_END_DATE)
 SELECT 'CONCEPT_CODE_1' || ','
     || 'CONCEPT_NAME_1' || ','
     || 'VOCABULARY_ID_1' || ','
@@ -22,23 +37,23 @@ SELECT 'CONCEPT_CODE_1' || ','
     || 'RELATIONSHIP_ID' || ','
     || 'VALID_START_DATE' || ','
     || 'VALID_END_DATE' || ','
-    || 'INVALID_REASON'
+    || 'INVALID_REASON' AS "&3 DATA"
 FROM DUAL
 UNION ALL
-SELECT '"' || CONCEPT_CODE_1  || '",'
-    || '"' || CONCEPT_NAME_1 || '",'
-    || '"' || VOCABULARY_ID_1 || '",'
-    || '"' || INVALID_REASON_1 || '",'
-    || '"' || CONCEPT_CODE_2 || '",'
-    || '"' || CONCEPT_NAME_2 || '",'
-    || '"' || CONCEPT_CLASS_ID_2 || '",'
-    || '"' || VOCABULARY_ID_2 || '",'
-    || '"' || INVALID_REASON_2 || '",'
-    || '"' || RELATIONSHIP_ID || '",'
-    || '"' || TO_CHAR(VALID_START_DATE, 'YYYY-MM-DD HH24:MI:SS') || '",'
-    || '"' || TO_CHAR(VALID_END_DATE, 'YYYY-MM-DD HH24:MI:SS')  || '",'
-    || '"' || INVALID_REASON || '"'
-FROM &3
+SELECT '"' || mt.CONCEPT_CODE_1  || '",'
+    || '"' || mt.CONCEPT_NAME_1 || '",'
+    || '"' || mt.VOCABULARY_ID_1 || '",'
+    || '"' || mt.INVALID_REASON_1 || '",'
+    || '"' || mt.CONCEPT_CODE_2 || '",'
+    || '"' || mt.CONCEPT_NAME_2 || '",'
+    || '"' || mt.CONCEPT_CLASS_ID_2 || '",'
+    || '"' || mt.VOCABULARY_ID_2 || '",'
+    || '"' || mt.INVALID_REASON_2 || '",'
+    || '"' || mt.RELATIONSHIP_ID || '",'
+    || '"' || mt.VALID_START_DATE || '",'
+    || '"' || mt.VALID_END_DATE || '",'
+    || '"' || mt.INVALID_REASON || '"'
+FROM mt
 ;
 
 SPOOL OFF
