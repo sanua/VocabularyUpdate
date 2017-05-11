500e44d2477c8ff1dbdded99001944f4cc6bb844
Alexander N. Yatsenko <avatorv@gmail.com>
2017-05-11 18:59:00 +0300

    Clear the CONCEPT_RELATIONSHIP_MANUAL table as unused in HCPCS but affected on 'CheckManualTable' verification.

--- vocabulary_data/dev_hcpcs/manual_table_prepare.sql
+++ vocabulary_data/dev_hcpcs/manual_table_prepare.sql
@@ -26,7 +26,11 @@ BEGIN
 	END LOOP;
 END;
 /
+COMMIT;
 
+PROMPT Empty the 'concept_relationship_manual' table. 
+PROMPT It''s not used in HCPCS but can lead to faulire in 'CheckManualTable'...
+TRUNCATE TABLE concept_relationship_manual;
 COMMIT;
 
 SPOOL OFF
